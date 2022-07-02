59df867c6b3b6a2c259522acebeee6005620a273
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-07-02 07:59:26 +0200

    update admin users

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -3,15 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Users;
-
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -20,51 +15,18 @@ class UsersCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Users::class;
-
-    }
-
-   
-
-
-    public function configureCrud(Crud $crud): Crud
-    {
-        return $crud 
-            ->setEntityLabelInPlural('Utilisateurs')
-            ->setEntityLabelInSingular('Utilisateur')
-
-            ->setPageTitle("index", "ECOIT - Administration des utilisateurs")
-
-            ->setPaginatorPageSize(10);
-
-    }
-
-     public function configureFilters(Filters $filters): Filters
-   {
-        return $filters
-            ->add(EntityFilter::new('roles'))
-        ;
     }
 
     
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')
-            ->hideOnForm(),
+            IdField::new('id'),
             TextField::new('lastname'),
-            TextField::new('firstname'),
-
-            // TextEditorField::new('description'),
-            TextField::new('pseudo'),
-            TextField::new('email')
-                ->hideOnForm(),
+            //TextEditorField::new('description'),
             ArrayField::new('roles'),
-                // ->hideOnIndex(),
-            DateTimeField::new('createdAt')
-                ->hideOnForm(),
-            BooleanField::new('is_verified'),
-                
+             BooleanField::new('is_verified'),
         ];
     }
-   
+    
 }
